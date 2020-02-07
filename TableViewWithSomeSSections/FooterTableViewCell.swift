6d79abc44699a13eadc05d5578e857fbdaf7b54e
Hajji Anwer <hajjianwer@mehdis-Mac-mini.local>
2020-02-07 18:01:18 +0100

    tableViewMultiSection

---
+++ TableViewWithSomeSSections/FooterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FooterTableViewCell.swift
+//  TableViewWithSomeSSections
+//
+//  Created by Hajji Anwer on 2/7/20.
+//  Copyright © 2020 Hajji Anwer. All rights reserved.
+//
+
+import UIKit
+
+class FooterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
